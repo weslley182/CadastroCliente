3dff2b62b63f978af7ee7059fed46f789b743cfc
Wesley Machado <weslley182@gmail.com>
2016-04-23 15:43:49 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/wesley/cadastroclientes/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.wesley.cadastroclientes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
